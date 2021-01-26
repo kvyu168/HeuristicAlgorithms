9cfd62ca3e473f0d716462215497647d5e8857ce
unknown <vbjc5275@gmail.com>
2021-01-26 14:36:39 +0800

    fix

--- ant_system.py
+++ ant_system.py
@@ -44,7 +44,7 @@ class AntSystem:
         self.pheromone_drop_amount = pheromone_drop_amount
         self.evaporate_rate = evaporate_rate
         self.pheromone_factor = pheromone_factor
-        self.heuristic_factor = heuristic_factor
+        self.visibility_factor = heuristic_factor
         
     def initialize(self):
         self.one_solution = np.arange(self.num_cities,dtype=int)
@@ -57,15 +57,15 @@ class AntSystem:
         self.best_solution = np.zeros(self.num_cities,dtype=int)
         self.best_objective_value = sys.float_info.max
         
-        self.heuristic_values = np.zeros((self.num_cities,self.num_cities))
-        self.pheromone_map = np.zeros((self.num_cities,self.num_cities))
+        self.visibility = np.zeros((self.num_cities,self.num_cities))
+        self.pheromone_map = np.ones((self.num_cities,self.num_cities))
         
         #heuristic_values
         for from_  in range(self.num_cities):
             for to in range(self.num_cities):
                 if(from_==to):continue
                 distance = self.get_distance(self.coordinate[from_],self.coordinate[to])
-                self.heuristic_values[from_][to] = 1/distance
+                self.visibility[from_][to] = 1/distance
      
     def do_roulette_wheel_selection(self,fitness_list):
         sum_fitness = sum(fitness_list)
@@ -101,15 +101,15 @@ class AntSystem:
             #best
             fitness_list = []
             for city_id in candidates:
-                fitness = pow(self.pheromone_map[current_city_id][city_id],self.pheromone_factor)+\
-                    pow(self.heuristic_values[current_city_id][city_id],self.heuristic_factor)
+                fitness = pow(self.pheromone_map[current_city_id][city_id],self.pheromone_factor)*\
+                    pow(self.visibility[current_city_id][city_id],self.visibility_factor)
                 fitness_list.append(fitness)
             
             next_city_id = candidates[self.do_roulette_wheel_selection(fitness_list)]
+            candidates.remove(next_city_id)
             self.one_solution[t] = next_city_id
-            current_city_id = next_city_id
-            candidates.remove(current_city_id)
             
+            current_city_id = next_city_id
         self.one_solution[-1] = candidates.pop()
 
     def each_ant_construct_its_solution(self):
